7ace9e21e79d29245c08cf93dfac366c7dbe4303
Yudhistira Gilang <yudhistiragilang22@gmail.com>
2020-02-20 01:48:10 +0700

    dashboard

--- home.php
+++ home.php
@@ -176,8 +176,13 @@ $page_content = "Beranda";
           </div>
 
           <?php if($roleUser == 1) :?>
-          <div class="row">
 
+          <div class="row">
+            <div class="col-md-12">
+              <h3>Maintenance</h3>
+            </div>
+          </div>
+          <div class="row">
             <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
@@ -230,7 +235,11 @@ $page_content = "Beranda";
           <?php endif; ?>
 
           <div class="row">
-
+            <div class="col-md-12">
+              <h3>Transaction</h3>
+            </div>
+          </div>
+          <div class="row">
             <div class="col-xl-3 col-md-6 mb-4">
               <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
