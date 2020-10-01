2e03be0ad77ca95806e3224f9bc301469512930e
Yudistira Gilang Adisetyo <yudhistiragilang22@gmail.com>
2020-10-01 21:32:11 +0700

    Update Connection.php

--- database/Connection.php
+++ database/Connection.php
@@ -18,16 +18,12 @@
 		{
 			
 			$this->conn = null;
-
 			try {
 				$this->conn = new PDO("mysql:host=".$this->servername.";dbname=".$this->database, $this->username, $this->password);
 				$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			}
-			catch(PDOException $e)
-			{
+			}catch(PDOException $e){
 				echo "Connection failed: " . $e->getMessage();
 			}
-			
 			return $this->conn;
 
 		}
